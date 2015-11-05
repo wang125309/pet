34f5dbfb3fffb15c98f8b00e2e45be78db427c60
wang125309 <wang_125309@foxmail.com>
2015-11-05 21:34:51 +0800

    push

--- static/js-modify/index.js
+++ static/js-modify/index.js
@@ -121,6 +121,42 @@ window.onload = function(){
             });
         }
     });
+    if(localStorage['date']) {
+                            $.post("/portal/wxconfig/",{
+                                "url":location.href
+                            },function(data){
+                                wx.config(data);
+                                if(window.shareUserId != undefined) {
+                                    link = "http://11.yanyr.com?userId="+window.shareUserId;
+                                }
+                                else {
+                                    link = "http://11.yanyr.com";
+                                }
+                                var shareJson = {
+                                    link:link,
+                                    imgUrl:"http://11.yanyr.com/static/image/share-image.jpg",
+                                    title:"逃离“双十一”的最佳方式:送你一只拉布拉多宝宝",
+                                    desc:"逃离“双十一”的最佳方式:送你一只拉布拉多宝宝宝"
+                                };
+                                wx.ready(function(){
+                                    wx.onMenuShareTimeline(shareJson);
+                                    wx.onMenuShareAppMessage(shareJson);
+                                });
+                                wx.error(function(res){
+                                    $.get("/portal/update_access_token/",function(data){
+                                        $.post("/portal/wxconfig/",{
+                                            "url":location.href
+                                        },function(data){
+                                            wx.config(data);
+                                            wx.ready(function(){
+                                                wx.onMenuShareTimeline(shareJson);
+                                                wx.onMenuShareAppMessage(shareJson);
+                                            });
+                                        });
+                                    });
+                                });
+                            });
+    }
     window.shareUserId = '';
     $(".page5 .submit").on('tap',function(){
         if(!localStorage['date']) {
--- static/js/.js
+++ static/js/.js
@@ -1902,6 +1902,42 @@ window.onload = function(){
             });
         }
     });
+    if(localStorage['date']) {
+                            $.post("/portal/wxconfig/",{
+                                "url":location.href
+                            },function(data){
+                                wx.config(data);
+                                if(window.shareUserId != undefined) {
+                                    link = "http://11.yanyr.com?userId="+window.shareUserId;
+                                }
+                                else {
+                                    link = "http://11.yanyr.com";
+                                }
+                                var shareJson = {
+                                    link:link,
+                                    imgUrl:"http://11.yanyr.com/static/image/share-image.jpg",
+                                    title:"逃离“双十一”的最佳方式:送你一只拉布拉多宝宝",
+                                    desc:"逃离“双十一”的最佳方式:送你一只拉布拉多宝宝宝"
+                                };
+                                wx.ready(function(){
+                                    wx.onMenuShareTimeline(shareJson);
+                                    wx.onMenuShareAppMessage(shareJson);
+                                });
+                                wx.error(function(res){
+                                    $.get("/portal/update_access_token/",function(data){
+                                        $.post("/portal/wxconfig/",{
+                                            "url":location.href
+                                        },function(data){
+                                            wx.config(data);
+                                            wx.ready(function(){
+                                                wx.onMenuShareTimeline(shareJson);
+                                                wx.onMenuShareAppMessage(shareJson);
+                                            });
+                                        });
+                                    });
+                                });
+                            });
+    }
     window.shareUserId = '';
     $(".page5 .submit").on('tap',function(){
         if(!localStorage['date']) {
@@ -1922,40 +1958,40 @@ window.onload = function(){
                             $(".share .like .count").html(data.like);
                             $(".share .rank .count").html(data.rank);
                             window.shareUserId = data.id;
-    $.post("/portal/wxconfig/",{
-		"url":location.href
-	},function(data){
-		wx.config(data);
-        if(window.shareUserId != undefined) {
-            link = "http://11.yanyr.com?userId="+window.shareUserId;
-        }
-        else {
-            link = "http://11.yanyr.com";
-        }
-        var shareJson = {
-            link:link,
-            imgUrl:"http://11.yanyr.com/static/image/share-image.jpg",
-            title:"逃离“双十一”的最佳方式:送你一只拉布拉多宝宝",
-            desc:"逃离“双十一”的最佳方式:送你一只拉布拉多宝宝宝"
-        };
-		wx.ready(function(){
-			wx.onMenuShareTimeline(shareJson);
-			wx.onMenuShareAppMessage(shareJson);
-        });
-		wx.error(function(res){
-			$.get("/portal/update_access_token/",function(data){
-				$.post("/portal/wxconfig/",{
-					"url":location.href
-				},function(data){
-					wx.config(data);
-					wx.ready(function(){
-                        wx.onMenuShareTimeline(shareJson);
-                        wx.onMenuShareAppMessage(shareJson);
-		            });
-		        });
-		    });
-        });
-    });
+                            $.post("/portal/wxconfig/",{
+                                "url":location.href
+                            },function(data){
+                                wx.config(data);
+                                if(window.shareUserId != undefined) {
+                                    link = "http://11.yanyr.com?userId="+window.shareUserId;
+                                }
+                                else {
+                                    link = "http://11.yanyr.com";
+                                }
+                                var shareJson = {
+                                    link:link,
+                                    imgUrl:"http://11.yanyr.com/static/image/share-image.jpg",
+                                    title:"逃离“双十一”的最佳方式:送你一只拉布拉多宝宝",
+                                    desc:"逃离“双十一”的最佳方式:送你一只拉布拉多宝宝宝"
+                                };
+                                wx.ready(function(){
+                                    wx.onMenuShareTimeline(shareJson);
+                                    wx.onMenuShareAppMessage(shareJson);
+                                });
+                                wx.error(function(res){
+                                    $.get("/portal/update_access_token/",function(data){
+                                        $.post("/portal/wxconfig/",{
+                                            "url":location.href
+                                        },function(data){
+                                            wx.config(data);
+                                            wx.ready(function(){
+                                                wx.onMenuShareTimeline(shareJson);
+                                                wx.onMenuShareAppMessage(shareJson);
+                                            });
+                                        });
+                                    });
+                                });
+                            });
                         });
                         $(".share").show();
                     }
