6612a1eeb3cbae0dd4c17e78961aecd69464e661
wang125309 <wang_125309@foxmail.com>
2015-11-06 13:26:21 +0800

    push

--- static/js-modify/share.js
+++ static/js-modify/share.js
@@ -13,7 +13,7 @@ $(function(){
             link:link,
             imgUrl:"http://11.yanyr.com/static/image/share-image.jpg",
             title:"逃离“双十一”的最佳方式:送你一只拉布拉多宝宝",
-            desc:"逃离“双十一”的最佳方式:送你一只拉布拉多宝宝宝"
+            desc:"双11来了，大家怎么过节？剁手的剁手，看晚会的看晚会，我就要小白养宠的那只狗狗！！"
         };
 		wx.ready(function(){
 			wx.onMenuShareTimeline(shareJson);
--- static/js/.js
+++ static/js/.js
@@ -1919,7 +1919,7 @@ window.onload = function(){
                                     link:link,
                                     imgUrl:"http://11.yanyr.com/static/image/share-image.jpg",
                                     title:"逃离“双十一”的最佳方式:送你一只拉布拉多宝宝",
-                                    desc:"逃离“双十一”的最佳方式:送你一只拉布拉多宝宝宝"
+                                    desc:"双11来了，大家怎么过节？剁手的剁手，看晚会的看晚会，我就要小白养宠的那只狗狗！！"
                                 };
                                 wx.ready(function(){
                                     wx.onMenuShareTimeline(shareJson);
@@ -1976,7 +1976,7 @@ window.onload = function(){
                                     link:link,
                                     imgUrl:"http://11.yanyr.com/static/image/share-image.jpg",
                                     title:"逃离“双十一”的最佳方式:送你一只拉布拉多宝宝",
-                                    desc:"逃离“双十一”的最佳方式:送你一只拉布拉多宝宝宝"
+                                    desc:"双11来了，大家怎么过节？剁手的剁手，看晚会的看晚会，我就要小白养宠的那只狗狗！！"
                                 };
                                 wx.ready(function(){
                                     wx.onMenuShareTimeline(shareJson);
